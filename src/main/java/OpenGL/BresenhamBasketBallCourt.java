1f1a3a3df18552416354df4bd219e083a0855234
Rubens Pessoa <pessoadebarros@gmail.com>
2017-03-02 20:34:01 -0300

    doing great

--- src/main/java/OpenGL/BresenhamBasketBallCourt.java
+++ src/main/java/OpenGL/BresenhamBasketBallCourt.java
@@ -3,6 +3,7 @@ package OpenGL;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.glu.GLUquadric;
 
 /**
  * Created by rubenspessoa on 02/03/17.
@@ -18,11 +19,63 @@ public class BresenhamBasketBallCourt implements GLEventListener {
     }
 
     public void display(GLAutoDrawable glAutoDrawable) {
+
         final GL2 gl = glAutoDrawable.getGL().getGL2();
 
-        gl.glBegin(GL2.GL_LINES);
-            gl.glVertex3f(-0.50f, 0.50f, 0);
-            gl.glVertex3f(0.50f, -0.50f,0);
+        //drawing top
+        gl.glBegin ( GL2.GL_LINES );
+        gl.glVertex3f( -0.3f, 0.3f, 0 );
+        gl.glVertex3f( 0.3f,0.3f, 0 );
+        gl.glEnd();
+
+        //drawing bottom
+        gl.glBegin( GL2.GL_LINES );
+        gl.glVertex3f( -0.3f,-0.3f, 0 );
+        gl.glVertex3f( 0.3f,-0.3f, 0 );
+        gl.glEnd();
+
+        //drawing the right edge
+        gl.glBegin( GL2.GL_LINES );
+        gl.glVertex3f( -0.3f,0.3f, 0 );
+        gl.glVertex3f( -0.3f,-0.3f, 0 );
+        gl.glEnd();
+
+        //drawing the left edge
+        gl.glBegin( GL2.GL_LINES );
+        gl.glVertex3f( 0.3f,0.3f,0 );
+        gl.glVertex3f( 0.3f,-0.3f,0 );
+        gl.glEnd();
+
+        //building roof
+        //building lft dia
+        gl.glBegin( GL2.GL_LINES );
+        gl.glVertex3f( 0f,0.6f, 0 );
+        gl.glVertex3f( -0.3f,0.3f, 0 );
+        gl.glEnd();
+
+        //building rt dia
+        gl.glBegin( GL2.GL_LINES );
+        gl.glVertex3f( 0f,0.6f, 0 );
+        gl.glVertex3f( 0.3f,0.3f, 0 );
+        gl.glEnd();
+
+        //building door
+        //drawing top
+        gl.glBegin ( GL2.GL_LINES );
+        gl.glVertex3f( -0.05f, 0.05f, 0 );
+        gl.glVertex3f( 0.05f, 0.05f, 0 );
+        gl.glEnd();
+
+        //drawing the left edge
+        gl.glBegin ( GL2.GL_LINES );
+        gl.glVertex3f( -0.05f, 0.05f, 0 );
+        gl.glVertex3f( -0.05f, -0.3f, 0 );
+        gl.glEnd();
+
+        //drawing the right edge
+        gl.glBegin ( GL2.GL_LINES );
+        gl.glVertex3f( 0.05f, 0.05f, 0 );
+        gl.glVertex3f( 0.05f, -0.3f, 0 );
         gl.glEnd();
 
     }
--- src/main/java/OpenGL/SimpleBasketBallCourt.java
+++ src/main/java/OpenGL/SimpleBasketBallCourt.java
@@ -16,11 +16,23 @@ public class SimpleBasketBallCourt implements GLEventListener {
     }
 
     public void display(GLAutoDrawable glAutoDrawable) {
+
         final GL2 gl = glAutoDrawable.getGL().getGL2();
 
         gl.glBegin(GL2.GL_LINES);
-            gl.glVertex3f(-1, -1, 0);
-            gl.glVertex3f(0, 0,0);
+        gl.glVertex3f(-1, 0, 0);
+        gl.glVertex3f(1, 0,0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+
+        for(int i =0; i <= 300; i++){
+            double angle = 2 * Math.PI * i / 300;
+            double x = Math.cos(angle) * 0.2;
+            double y = Math.sin(angle) * 0.2;
+            gl.glVertex2d(x,y);
+        }
+
         gl.glEnd();
     }
 
